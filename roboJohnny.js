d37cfa2842f977e0df65cc1e909f9db194537dcb
Phil Rajchgot <tophil@outlook.com>
2016-03-12 20:20:01 -0500

    started to code motor control functionality using johnny-five/raspi-io

--- roboJohnny.js
+++ roboJohnny.js
@@ -1,29 +1,63 @@
+var command = require('./robo-commands.js');
 var five = require("johnny-five");
 var Raspi = require("raspi-io");
 var board = new five.Board({
   io: new Raspi()
 });
+// pull motor configs from ADAFRUIT_V2 template
+var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V2;
+// instantiate motors
+var m1 = new five.Motor(configs.M1);
+var m2 = new five.Motor(configs.M2);
 
-var m1 = new five.Motor({
-  pins: {
-    pwm: 8,
-    dir: 9,
-    cdir: 10
-  }
-});
+var io = require('socket.io-client')('http://localhost:3000', {query: 'type=robot'});
+//establish connection to server
+var socket = io.connect();
 
-var m2 = new five.Motor({
-  pins: {
-    pwm: 13,
-    dir: 12,
-    cdir: 11
-  }
-});
+var MAXSPEED = 160;
 
 board.on("ready", function() {
-  m1.start(32);
+  //listen for commands to robot
+  socket.on(command.COMMAND, function(msg) {
+    switch(msg) {
+      case command.FORWARD:
+        forward(MAXSPEED);
+        break;
+      case command.REVERSE:
 
-  board.wait(1000, function() {
-    m1.stop();
-  })
+        break;
+      case command.TURN_LEFT:
+
+        break;
+      case command.TURN_RIGHT:
+
+        break;
+      case command.STOP:
+        stop();
+        break;
+    }
+  });
 });
+
+var STEPS = 10;
+var TIMESTEP = 100;
+
+function forward(finalSpeed) {
+  var speed = 0;
+
+  var interval = setInterval(function() {
+    if (speed > finalSpeed) {
+      clearInterval(interval);
+      return;
+    }
+
+    speed += finalSpeed / STEPS;
+    m1.forward(speed);
+    m2.forward(speed);
+  }, TIMESTEP);
+}
+
+function stop() {
+  m1.stop();
+  m2.stop();
+}
