aee849d7affc2f146e0a17a548a0dc1faa69c094
Phil Rajchgot <tophil@outlook.com>
2016-03-12 20:27:52 -0500

    added all movement types

--- roboJohnny.js
+++ roboJohnny.js
@@ -14,23 +14,23 @@ var io = require('socket.io-client')('http://localhost:3000', {query: 'type=robo
 //establish connection to server
 var socket = io.connect();
 
-var MAXSPEED = 160;
+var SPEED = 64;
 
 board.on("ready", function() {
   //listen for commands to robot
   socket.on(command.COMMAND, function(msg) {
     switch(msg) {
       case command.FORWARD:
-        forward(MAXSPEED);
+        forward(SPEED);
         break;
       case command.REVERSE:
-
+        reverse(SPEED);
         break;
       case command.TURN_LEFT:
-
+        left(SPEED);
         break;
       case command.TURN_RIGHT:
-
+        right(SPEED);
         break;
       case command.STOP:
         stop();
@@ -39,25 +39,44 @@ board.on("ready", function() {
   });
 });
 
-var STEPS = 10;
-var TIMESTEP = 100;
+function forward (speed) {
+  m1.fwd(speed);
+  m2.fwd(speed);
+}
 
-function forward(finalSpeed) {
-  var speed = 0;
+function reverse (speed) {
+  m1.rev(speed);
+  m2.rev(speed);
+}
 
-  var interval = setInterval(function() {
-    if (speed > finalSpeed) {
-      clearInterval(interval);
-      return;
-    }
+function left (speed) {
+  m1.rev(speed);
+  m2.fwd(speed);
+}
 
-    speed += finalSpeed / STEPS;
-    m1.forward(speed);
-    m2.forward(speed);
-  }, TIMESTEP);
+function right (speed) {
+  m1.fwd(speed);
+  m2.rev(speed);
 }
 
 function stop() {
   m1.stop();
   m2.stop();
 }
+// var STEPS = 10;
+// var TIMESTEP = 100;
+//
+// function forward(finalSpeed) {
+//   var speed = 0;
+//
+//   var interval = setInterval(function() {
+//     if (speed > finalSpeed) {
+//       clearInterval(interval);
+//       return;
+//     }
+//
+//     speed += finalSpeed / STEPS;
+//     m1.forward(speed);
+//     m2.forward(speed);
+//   }, TIMESTEP);
+// }
